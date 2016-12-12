ab0f54801f7926830b2d51717a8cd37531cfc63b
Andy Bell <andyrbell@gmail.com>
2016-12-12 21:35:24 +0000

    Day 7 part 2

--- src/main/java/Day07.java
+++ src/main/java/Day07.java
@@ -1,13 +1,15 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by abell on 12/12/16.
  */
 public class Day07 {
-    static boolean valid(String ip) {
+    static boolean supportsTls(String ip) {
 
         boolean foundOutsideBrackets = false;
         boolean foundInsideBrackets = false;
@@ -48,8 +50,52 @@ public class Day07 {
         return chars[i] == chars[i + 3] && chars[i + 1] == chars[i + 2] && chars[i] != chars[i + 1];
     }
 
+    private static boolean isAbaOrBab(char[] chars, int i) {
+        return chars[i] == chars[i + 2] && chars[i] != chars[i + 1];
+    }
+
+    static boolean supportsSsl(String ip) {
+        Map<String, String> abaToBab = new HashMap<>();
+        Map<String, String> babToAba = new HashMap<>();
+
+        int openBrackets = 0;
+
+        char[] chars = ip.toCharArray();
+        for (int i = 0; i < chars.length - 2; i++) {
+
+            if (chars[i] == '[') {
+                openBrackets++;
+                continue;
+            }
+
+            if (chars[i] == ']') {
+                if (openBrackets > 0) {
+                    openBrackets--;
+                }
+                continue;
+            }
+
+            if (isAbaOrBab(chars, i)) {
+                String matched = String.valueOf(chars, i, 3);
+                char[] rev = new char[3];
+                rev[0] = chars[i + 1];
+                rev[1] = chars[i];
+                rev[2] = chars[i + 1];
+                String reversed = new String(rev);
+                if (openBrackets > 0) {
+                    babToAba.put(matched, reversed);
+                } else {
+                    abaToBab.put(matched, reversed);
+                }
+            }
+        }
+
+        return abaToBab.values().stream().filter(babToAba::containsKey).count() > 0;
+    }
+
     public static void main(String[] args) throws IOException {
         List<String> lines = Files.readAllLines(Paths.get("src/main/resources/Day07.txt"));
-        System.out.println("Part one answer: " + lines.stream().filter(Day07::valid).count());
+        System.out.println("Part one answer: " + lines.stream().filter(Day07::supportsTls).count());
+        System.out.println("Part two answer: " + lines.stream().filter(Day07::supportsSsl).count());
     }
 }
--- src/test/java/Day07Test.java
+++ src/test/java/Day07Test.java
@@ -10,24 +10,32 @@ public class Day07Test {
     @Test
     public void example1() throws Exception {
         String ip = "abba[mnop]qrst";
-        assertEquals(true, Day07.valid(ip));
+        assertEquals(true, Day07.supportsTls(ip));
     }
 
     @Test
     public void example2() throws Exception {
         String ip = "abcd[bddb]xyyx";
-        assertEquals(false, Day07.valid(ip));
+        assertEquals(false, Day07.supportsTls(ip));
     }
 
     @Test
     public void example3() throws Exception {
         String ip = "aaaa[qwer]tyui";
-        assertEquals(false, Day07.valid(ip));
+        assertEquals(false, Day07.supportsTls(ip));
     }
 
     @Test
     public void example4() throws Exception {
         String ip = "ioxxoj[asdfgh]zxcvbn";
-        assertEquals(true, Day07.valid(ip));
+        assertEquals(true, Day07.supportsTls(ip));
+    }
+
+    @Test
+    public void sslExample1() throws Exception {
+        String ip = "xyx[yxy]abc";
+
+        assertEquals(true, Day07.supportsSsl(ip));
+
     }
 }
