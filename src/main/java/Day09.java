7bb7a8de6c3c6ae73f6da8bfbbba7c4b9e6c458a
Andy Bell <andyrbell@gmail.com>
2016-12-22 18:46:37 +0000

    Day 9 part 2

--- src/main/java/Day09.java
+++ src/main/java/Day09.java
@@ -1,62 +1,53 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by abell on 16/12/16.
  */
 public class Day09 {
-    public static String decompress(String input) {
-        char[] chars = input.toCharArray();
 
-        StringBuilder markerBuffer = new StringBuilder();
-        StringBuilder contentBuffer = new StringBuilder();
-        boolean readingMarker = false;
+    static Pattern pattern = Pattern.compile("\\(([\\d]+)x([\\d]+)\\)");
 
-        int i = 0;
-        while (i < chars.length) {
+    static long decompress(String input) {
+        return decompress(input, false);
+    }
 
-            if (chars[i] == '(') {
-                readingMarker = true;
-                i++;
-                continue;
-            }
+    static long decompress(String input, boolean recursive) {
+        return decompress(input, 0, input.length() - 1, recursive);
+    }
 
-            if (readingMarker && chars[i] == ')') {
-                String[] marker = markerBuffer.toString().split("x");
-                int markerNoOfChars = Integer.parseInt(marker[0]);
-                int markerRepeat = Integer.parseInt(marker[1]);
+    static long decompress(String input, int fromIndex, int toIndex, boolean recursive) {
 
-                char[] decompressedContent = Arrays.copyOfRange(chars, i + 1, i + 1 + markerNoOfChars);
+        Matcher matcher = pattern.matcher(input);
+        long length = 0;
 
-                for (int j = 0; j < markerRepeat; j++) {
-                    contentBuffer.append(decompressedContent);
-                }
-                readingMarker = false;
-                markerBuffer.delete(0, markerBuffer.length());
-                i += markerNoOfChars + 1;
-                continue;
-            }
+        while (matcher.find(fromIndex) && matcher.start() < toIndex) {
 
-            if (readingMarker) {
-                markerBuffer.append(chars[i]);
-            } else {
-                contentBuffer.append(chars[i]);
-            }
+            int markerNoOfChars = Integer.parseInt(matcher.group(1));
+            int markerRepeat = Integer.parseInt(matcher.group(2));
 
-            i++;
+            length += matcher.start() - fromIndex;
+
+            long decompressedLength = recursive ? decompress(input, matcher.end(), matcher.end() + markerNoOfChars - 1, true) : markerNoOfChars;
+
+            length += decompressedLength * markerRepeat;
+
+            fromIndex = matcher.end() + markerNoOfChars;
         }
 
-        System.out.println("contentBuffer length: " + contentBuffer.length());
+        length += toIndex - fromIndex + 1;
 
-        return contentBuffer.toString();
+        return length;
     }
 
     public static void main(String[] args) throws IOException {
         List<String> lines = Files.readAllLines(Paths.get("src/main/resources/Day09.txt"));
 
-        System.out.println("Part one answer: " + decompress(lines.get(0)).length());
+        System.out.println("Part one answer: " + decompress(lines.get(0)));
+        System.out.println("Part two answer: " + decompress(lines.get(0), true));
     }
 }
--- src/test/java/Day09Test.java
+++ src/test/java/Day09Test.java
@@ -9,35 +9,51 @@ public class Day09Test {
 
     @Test
     public void example1() throws Exception {
-        test("ADVENT", "ADVENT");
+        testLength("ADVENT", 6, false);
     }
 
     @Test
     public void example2() throws Exception {
-        test("A(1x5)BC", "ABBBBBC");
+        testLength("A(1x5)BC", 7, false);
     }
 
     @Test
     public void example3() throws Exception {
-        test("(3x3)XYZ", "XYZXYZXYZ");
+        testLength("(3x3)XYZ", 9, false);
     }
 
     @Test
     public void example4() throws Exception {
-        test("A(2x2)BCD(2x2)EFG", "ABCBCDEFEFG");
+        testLength("A(2x2)BCD(2x2)EFG", 11, false);
     }
 
     @Test
     public void example5() throws Exception {
-        test("(6x1)(1x3)A", "(1x3)A");
+        testLength("(6x1)(1x3)A", 6, false);
     }
 
     @Test
     public void example6() throws Exception {
-        test("X(8x2)(3x3)ABCY", "X(3x3)ABC(3x3)ABCY");
+        testLength("X(8x2)(3x3)ABCY", 18, false);
     }
 
-    public void test(String input, String expected) throws Exception {
-        assertEquals(expected, Day09.decompress(input));
+    @Test
+    public void example7() throws Exception {
+        testLength("X(8x2)(3x3)ABCY", 20, true);
     }
+
+    @Test
+    public void example8() throws Exception {
+        testLength("(27x12)(20x12)(13x14)(7x10)(1x12)A", 241920, true);
+    }
+
+    @Test
+    public void example9() throws Exception {
+        testLength("(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN", 445, true);
+    }
+
+    private void testLength(String input, int expected, boolean recursive) throws Exception {
+        assertEquals(expected, Day09.decompress(input, recursive));
+    }
+
 }
